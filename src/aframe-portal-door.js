3348a7851405da29f8cdc16d0f22cfd78066a4b4
WilKhalifa <43878318+WilKhalifa@users.noreply.github.com>
2019-04-26 17:51:52 +0200

    event AFRAME

--- src/aframe-portal-door.js
+++ src/aframe-portal-door.js
@@ -1,6 +1,23 @@
 //////////////////////////////////////////////////////////////////////////////
 //		arjs-hit-testing
 //////////////////////////////////////////////////////////////////////////////
+AFRAME.registerComponent('registerevents', {
+		init: function () {
+			var marker = this.el;
+			marker.addEventListener('markerFound', function() {
+				var markerId = marker.id;
+				console.log('markerFound', markerId);
+				// TODO: Add your own code here to react to the marker being found.
+			});
+			marker.addEventListener('markerLost', function() {
+				var markerId = marker.id;
+				console.log('markerLost', markerId);
+				// TODO: Add your own code here to react to the marker being lost.
+			});
+		}
+	});
+
+
 AFRAME.registerComponent('arjs-portal-door', {
 	schema: {
 		url : {		// Url of the content - may be video or image
--- src/threex-portal-door.js
+++ src/threex-portal-door.js
@@ -51,21 +51,6 @@ THREEx.Portal360 = function(videoImageURL, doorWidth, doorHeight){
 	doorCenter.add(frameMesh)
 }
 
-AFRAME.registerComponent('registerevents', {
-		init: function () {
-			var marker = this.el;
-			marker.addEventListener('markerFound', function() {
-				var markerId = marker.id;
-				console.log('markerFound', markerId);
-				// TODO: Add your own code here to react to the marker being found.
-			});
-			marker.addEventListener('markerLost', function() {
-				var markerId = marker.id;
-				console.log('markerLost', markerId);
-				// TODO: Add your own code here to react to the marker being lost.
-			});
-		}
-	});
 
 
 //////////////////////////////////////////////////////////////////////////////
