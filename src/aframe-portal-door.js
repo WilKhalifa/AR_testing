2402d061e929c343a507c39ee785f16dc01896a6
WilKhalifa <43878318+WilKhalifa@users.noreply.github.com>
2019-04-29 13:51:59 +0200

    second commit

--- src/aframe-portal-door.js
+++ src/aframe-portal-door.js
@@ -1,23 +1,6 @@
 //////////////////////////////////////////////////////////////////////////////
 //		arjs-hit-testing
 //////////////////////////////////////////////////////////////////////////////
-AFRAME.registerComponent('registerevents', {
-		init: function () {
-			var marker = this.el;
-			marker.addEventListener('markerFound', function() {
-				var markerId = marker.id;
-				console.log('markerFound', markerId);
-				// TODO: Add your own code here to react to the marker being found.
-			});
-			marker.addEventListener('markerLost', function() {
-				var markerId = marker.id;
-				console.log('markerLost', markerId);
-				// TODO: Add your own code here to react to the marker being lost.
-			});
-		}
-	});
-
-
 AFRAME.registerComponent('arjs-portal-door', {
 	schema: {
 		url : {		// Url of the content - may be video or image
